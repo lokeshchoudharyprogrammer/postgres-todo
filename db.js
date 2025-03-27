11509fbed0441ede462cad6bdfec4a3e47ad55da
Lokesh Choudhary Programmer <112642820+lokeshchoudharyprogrammer@users.noreply.github.com>
2025-03-27 17:53:58 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const {Pool}= require("pg");
 
-const pool=new Pool({connectionString:'postgresql://todo_app_owner:npg_Zm5sJ4QvzFkC@ep-autumn-heart-a5d78avo-pooler.us-east-2.aws.neon.tech/todo_app?sslmode=require'
+const pool=new Pool({connectionString:process.env.DBURL
 })
 
 
